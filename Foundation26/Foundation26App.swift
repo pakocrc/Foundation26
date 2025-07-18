f0ee89ec5826bf881076dce67f0ccdd5f73d4051
Francisco Cordoba <pako.cordoba10@gmail.com>
2025-07-18 13:58:27 -0600

    Initial commit

---
+++ Foundation26/Foundation26App.swift
@@ -0,0 +1,17 @@
+//
+//  Foundation26App.swift
+//  Foundation26
+//
+//  Created by Francisco Cordoba on 18/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Foundation26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
